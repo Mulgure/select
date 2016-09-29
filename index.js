a7a72cffe2675c25c5a93bee6005c4b39a4319e6
Mulgure <Mulgure@users.noreply.github.com>
2016-09-29 16:48:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
    content=document.getElementById('content')
-   content.style.display="none";           //默认不显示
+   content.style.display="none";          //默认不显示
+
+   ip1=document.getElementById("ip1")
 
 	for(i=0;i<document.getElementsByClassName('newOption').length;i++){
 		 document.getElementsByClassName('newOption')[i].onclick=function(){
 			document.getElementById('ip1').value = this.innerHTML;
 		}
 	}
-	function myFunction(){		
+
+	ip1.onInput = function(){		
 		for(j=0;j<document.getElementsByClassName('newOption').length;j++){								
 			document.getElementsByClassName('newOption')[j].style.display="none";		//重置
 		}
@@ -15,14 +18,14 @@
 			if(document.getElementsByClassName('newOption')[i].innerHTML.indexOf(document.getElementById('ip1').value)===0){
 
 				document.getElementsByClassName('newOption')[i].style.display="block";   //前缀匹配输入	
-			} 
+			}
 		}	
 	}
 
-	function myFunction2(){							//获取焦点后列表全显示
+	ip1.onfocus = function(){							//获取焦点后列表全显示
 		content.style.display="block";
 	}	
 	
-	function myFunction3(){							//失去焦点后列表消失 要兼顾点击事件，所以用定时器
+	ip1.onblur = function(){							//失去焦点后列表消失 要兼顾点击事件，所以用定时器
 		setTimeout('content.style.display="none"',100)
 	}
