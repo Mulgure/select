5ae04afa2f9ee6f24358b52209fdbaf499d52bcf
Mulgure <Mulgure@users.noreply.github.com>
2016-10-02 00:34:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
-   content=document.getElementById('content')
-   content.style.display="none";          //默认不显示
+    var myOption=document.getElementsByClassName('newOption')     
+    var content=document.getElementById('content')
+	var myInput=document.getElementById("myInput")
 
-   ip1=document.getElementById("ip1")
-
-	for(i=0;i<document.getElementsByClassName('newOption').length;i++){
-		 document.getElementsByClassName('newOption')[i].onclick=function(){
-			document.getElementById('ip1').value = this.innerHTML;
+	for(var i=0;i<myOption.length;i++){
+		myOption[i].onclick=function(){
+			myInput.value = this.innerHTML;
 		}
 	}
-	ip1.oninput=function(){		
-		for(j=0;j<document.getElementsByClassName('newOption').length;j++){								
-			document.getElementsByClassName('newOption')[j].style.display="none";		//重置
+	myInput.oninput=function(){		
+		for(var j=0;j<myOption.length;j++){								
+			myOption[j].style.display="none";		//重置
 		}
 
-		for(i=0;i<document.getElementsByClassName('newOption').length;i++){
-			if(document.getElementsByClassName('newOption')[i].innerHTML.indexOf(document.getElementById('ip1').value)===0){
+		for(var i=0;i<myOption.length;i++){
+			if(myOption[i].innerHTML.indexOf(myInput.value)===0){
 
-				document.getElementsByClassName('newOption')[i].style.display="block";   //前缀匹配输入	
+				myOption[i].style.display="block";   //前缀匹配输入	
 			} 
 		}	
 	}
 
-	ip1.onfocus=function(){							//获取焦点后列表全显示
+	myInput.onfocus=function(){							//获取焦点后列表全显示
 		content.style.display="block";
 	}	
 	
-	ip1.onblur=function(){							//失去焦点后列表消失 要兼顾点击事件，所以用定时器
+	myInput.onblur=function(){							//失去焦点后列表消失 要兼顾点击事件，所以用定时器
 		setTimeout('content.style.display="none"',100)
 	}
